9dab1e9d5e85c5843ca7fd31d767ae0d16db6330
verica02 <vspaskovska@gmail.com>
2022-06-25 19:06:17 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import Inputs from "./components/Inputs";
 import TopButtons from "./components/TopButtons";
 import './App.css';
-
+import getFormattedWeatherData from "./services/weatherService";
 import TimeAndLocation from "./components/TimeAndLocation";
 import TemperatureAndDetails from './components/TemperatureAndDetails';
 import Forecast from './components/Forecast';
 
 function App() {
+  
+  
+
+
+  //API call
+  const fetchWeather = async() => {
+  
+    const data = await getFormattedWeatherData({q: 'London'});//this is the query
+    console.log(data);
+
+  }
+
+  fetchWeather();
+
+  
   return (
     <div className = 'mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from-cyan-700 to-blue-700 h-fit shadow-xl shadow-gray-400'>
     <TopButtons />
